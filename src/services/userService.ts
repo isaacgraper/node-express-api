42374b5ea607383f207f9569c2f02baf6d4e49ad
Isaac Kleimmann Graper <isaac.graper@orbenk.br>
2024-09-23 08:39:58 -0300

    fix: repositories typos

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,15 +1,15 @@
-import { userModel } from "../models/user"; 
+import { userModel } from "../models/userModel"; 
 
 export class userService {
     async createUser(data: { username: string; email: string, password: string }) {
-        return userModel.create({ data })
+        return userModel.create({ data });
       }
     
     async getUser(id: number ) {
-        return userModel.findUnique({where: {id}});
+        return userModel.findUnique({ where: {id} });
     }
 
     async updateUser(id: number, data: { username: string, email: string}) {
-        return userModel.
+        return userModel.update({ where: {id}, data });
     }
 }
